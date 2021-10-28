c8b1ad873a1cd6bd246a74010d09c92bee197d00
ductai26998 <ductai26998@gmail.com>
2021-10-29 00:26:29 +0700

    get rss and show data of a website

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view /> -->
+    <!-- <div v-for="(item, index) in items" :key="index">
+      <h1>{{ item.title }}</h1>
+      <p>{{ item.link }}</p>
+      <p>{{ item.description }}</p>
+    </div> -->
+
+    <div class="articles">
+      <div class="articles_left">
+        <div
+          class="articles_left_item"
+          v-for="(article, index) in articles"
+          :key="index"
+        >
+          <!-- <img class="item-img" src={article.image} /> -->
+          <ArticleItem :article="article"> </ArticleItem>
+        </div>
+      </div>
+      <!-- <div class="articles_right">
+          <BrowserRouter>
+            <div class="category">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <NavLink to="/"
+                    activeclass="selected"
+                  ><span class="nav-item-text">LAST NEWS</span></NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink to="/"
+                  ><span class="nav-item-text">POPULAR</span></NavLink>
+                </li>
+                <li class="nav-item">
+                  <NavLink to="/"
+                  >TOPIC</NavLink>
+                </li>
+              </ul>
+            </div>
+          </BrowserRouter>
+          <div class="article-list">
+            {lastNewarticles.map(article => (
+              <div class="category-article-item" key={article.id}>
+                <img class="article-item-img" src={article.image} />
+                <articleItem
+                  title={article.title}
+                  release={article.release.slice(0, 10)}
+                  author={article.author}
+                ></articleItem>
+              </div>
+            ))}
+          </div>
+        </div> -->
+    </div>
+    <!-- <div class="loading show"><Loading /></div> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import ArticleItem from "../components/ArticleItem.vue"
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  components: {ArticleItem},
+  data() {
+    return {
+      articles: [],
+    };
   },
+  async created() {
+    const RSS_URL = `https://vass.gov.vn/noidung/rss/Lists/RssChannel/View_Detail.aspx?ItemID=27`;
+
+    await axios
+      .get(RSS_URL)
+      .then((response) => response.data)
+      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then((data) => {
+        const htmlItems = data.querySelectorAll("item");
+        htmlItems.forEach((item) => {
+          const title = this.getText(item.querySelector("title").innerHTML);
+          const link = this.getText(item.querySelector("link").innerHTML);
+          const description = this.getText(
+            item.querySelector("description").innerHTML
+          );
+          this.articles.push({
+            title: title,
+            link: link,
+            description: description,
+          });
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  },
+  beforeMount() {},
+  methods: {
+    getText(item) {
+      return item.slice(9, item.length - 3);
+    },
+  },
+  computed: {},
 };
 </script>
